7f41205f2ca779d5a17b14be92d0430d56e1628c
Till O <blackminer1242@gmail.com>
2022-01-28 23:42:05 +0100

    little Clean up

--- __main__.py
+++ __main__.py
@@ -43,54 +43,28 @@ class LectureObj:
 
         print(self.path)
 
-    def get_download_link(self):
-        return self.download_link
 
-    def get_path(self):
-        return self.path
-
-
-def get_course_lecture_ids(course_id: str) -> dict:
-    return_list = {}
-    response = requests.get(f"https://codewithmosh.com/courses/enrolled/{course_id}", headers=header)
-
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    s2 = soup.select('.col-sm-12')
-
-    for s1 in s2:
-        id_list = []
-        title = s1.find('div', class_='section-title').contents[2].getText().strip()
-        for s3 in s1.select("li"):
-            return_list[s3.get("data-lecture-id")] = title
-
-    return return_list
-
-
-def get_course_lecture_pure_ids(course_id: str) -> list:
+def get_course_lecture_ids(course_id: str) -> list:
     id_list = []
     response = requests.get(f"https://codewithmosh.com/courses/enrolled/{course_id}", headers=header)
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser").select('.col-sm-12')
 
-    s2 = soup.select('.col-sm-12')
-
-    for s1 in s2:
+    for s1 in soup:
         for s3 in s1.select("li"):
             id_list.append(s3.get("data-lecture-id"))
 
     return id_list
 
 
-def get_courses_ids() -> list:
+def get_course_ids() -> list:
     id_list = []
 
     response = requests.get("https://codewithmosh.com/courses/enrolled/240431", headers=header)
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser").select(".course-listing")
 
-    soup_select = soup.select(".course-listing")
-    for rq in soup_select:
+    for rq in soup:
         id_list.append(rq.get("data-course-id"))
 
     return id_list
@@ -99,17 +73,16 @@ def get_courses_ids() -> list:
 def get_lecture_download_link(lecture_id: str, course_id: str) -> str:
     response = requests.get(f"https://codewithmosh.com/courses/{course_id}/lectures/{lecture_id}", headers=header)
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser").select(".download")
 
-    soup_select = soup.select(".download")
     try:
-        return soup_select[0].get("href")
+        return soup[0].get("href")
     except IndexError:
         return ""
 
 
 def download_video(link: str, path=""):
-    start = time.time()
+    dl_start = time.time()
 
     if link == "":
         return
@@ -123,8 +96,6 @@ def download_video(link: str, path=""):
     if Path(path + file_name).is_file():
         return
 
-    print(path + file_name)
-
     with alive_bar(title=f'Downloading "{file_name}" ', bar='classic', monitor=False, elapsed=False) as bar:
         with open(path + file_name, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024 * 1024):
@@ -132,8 +103,8 @@ def download_video(link: str, path=""):
                     f.write(chunk)
                     bar()
                     f.flush()
-    end = time.time()
-    print(f"Done! File: {path + file_name} \nTime: {end - start:.2f} sec")
+    dl_end = time.time()
+    print(f"Finished Download in {dl_end - dl_start:.2f} sec! Path: {path + file_name} ")
 
 
 def ensure_dir(file_path):
@@ -143,9 +114,9 @@ def ensure_dir(file_path):
 
 
 def main():
-    for c_id in get_courses_ids():
-        for l_id in get_course_lecture_pure_ids(str(c_id)):
-            lct_obj = LectureObj(str(c_id), str(l_id))
+    for course_id in get_course_ids():
+        for lecture_id in get_course_lecture_ids(str(course_id)):
+            lct_obj = LectureObj(str(course_id), str(lecture_id))
             download_video(lct_obj.download_link, "output/" + lct_obj.path)
 
 
